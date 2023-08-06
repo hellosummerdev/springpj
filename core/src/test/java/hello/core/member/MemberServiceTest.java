65c0810105395c93a43c5a3ca1e7ce65e2b81e86
seulsummerlee <seullee@mindpro.co.kr>
2023-08-06 16:47:59 +0900

    commit order and discount
    feat : order and fixed amount discount

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -13,9 +13,8 @@ public class MemberServiceTest {
         Member member = new Member(1L, "memberA", Grade.VIP);
         //when
         memberService.join(member);
-        Member findMember = memberService.findMember(2L);
+        Member findMember = memberService.findMember(1L);
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
-
     }
 }
